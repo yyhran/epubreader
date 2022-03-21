2c9c52e821df09ccf930d9e809cca884a8df28f3
Entropy_s <53965806+Ang-ao@users.noreply.github.com>
2022-03-21 17:45:20 +0800

    -

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-#include <QPdfWriter>
 
 int main(int argc, char** argv)
 {
     ::std::cout << "Hi EPUB" << ::std::endl;
-    auto pdf = QPdfWriter("qrc:/test.epub");
     return 0;
 }
